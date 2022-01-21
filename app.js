ae7af1bed7d040bac78af81e890875ccb2b8a6ab
grassfinn <theharper@live.com>
2022-01-20 23:01:37 -0500

    Button that plays and stops audio

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-const song = document.querySelector('.song')
+song = document.querySelector('.song');
+song.volume = 0.6;
+function toggleMusic() {
+  return song.paused ? song.play() : song.pause();
+}
+var myAudio = document.getElementById('myAudio');
 
-song.volume = 0.5
+// https://stackoverflow.com/questions/27368778/how-to-toggle-audio-play-pause-with-one-button-or-link/38665865#comment43189469_27368778
